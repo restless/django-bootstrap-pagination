3fdd0640773e38ef99d813569ed3709e44d00271
Denilson Sá <denilsonsa@gmail.com>
2014-05-14 00:35:42 -0300

    Fixing broken code from commit 13226b950fc405317554075b3c080036935e186e

--- bootstrap_pagination/templatetags/bootstrap_pagination.py
+++ bootstrap_pagination/templatetags/bootstrap_pagination.py
@@ -177,7 +177,7 @@ class BootstrapPaginationNode(Node):
                 else:
                     range_max += shift
 
-        page_range = (range_min, range_max + 1)
+        page_range = range(range_min, range_max + 1)
 
         # Generate our URLs (page range + special urls for first, previous, next, and last)
         page_urls = []
